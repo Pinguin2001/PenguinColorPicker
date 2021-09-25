56b4a8677362733bcc7b2d6e3de812ffdbab9f26
Pinguin2001 <75797743+Pinguin2001@users.noreply.github.com>
2021-09-25 18:07:01 +0200

    Upload Source Code (v1.0)

---
+++ ColorPicker/AboutPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ColorPicker
+{
+    public sealed partial class AboutPage : Page
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+        private void CloseAbout_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+    }
+}
---
+++ ColorPicker/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ColorPicker
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OpenAbout_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(AboutPage), null);
+        }
+    }
+}
